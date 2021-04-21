74560069768d16bce84f616287319f884ebf44e6
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-04-21 16:04:27 +0200

    Add first if else

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,15 @@
 public class Calculator {
 
-    public static int add(int value1, int value2){
+    public static int add(int value1, int value2) {
         return value1 + value2;
+    }
+
+
+    public static String curfew(int value) {
+        if (value > 100) {
+            return "curfew";
+        } else {
+            return "no curfew";
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,17 +7,40 @@ public class CalculatorTest {
 
     @Test
     @DisplayName("add should return sum of values")
-    public void addShouldReturnSumOfValues(){
+    public void addShouldReturnSumOfValues() {
         //GIVEN
         int value1 = 3;
         int value2 = 5;
-        //some test push 2
 
         //WHEN
         int sum = Calculator.add(value1, value2);
 
         //THEN
         assertEquals(8, sum);
+    }
+
+    @Test
+    public void curfewShouldReturnCurfewWhenValueIsGreaterThan100() {
+        //GIVEN
+        int value = 101;
+
+        //WHEN
+        String curfew = Calculator.curfew(value);
+
+        //THEN
+        assertEquals("curfew", curfew);
+    }
+
+    @Test
+    public void curfewShouldReturnNoCurfewWhenValueIsLowerThan100() {
+        //GIVEN
+        int value = 100;
+
+        //WHEN
+        String curfew = Calculator.curfew(value);
+
+        //THEN
+        assertEquals("no curfew", curfew);
     }
 
 }
